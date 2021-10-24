5f72a042bd0fc3c3c5de62926dfff7038fef698d
github username <sharnellgrant@gmail.com>
2021-10-23 22:26:27 -0400

    commit tree 840b8cfa06dbe4481eecc9bb0f791c8a5264b3ae

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,44 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
+   handleUserNameChange = (event) => {
+     this.setState({
+       username: event.target.value,
+     });
+   }
+
+   handlePasswordChange = (event) => {
+     this.setState({
+        password: event.target.value,
+     });
+   }
+
+   handleSubmit = (event) => {
+     event.preventDefault();
+     //The onSubmit callback prop should only be called if both 
+     //fields are filled in (with any value).
+      if (!this.state.username || !this.state.password) return 
+       this.props.handleLogin(this.state) 
+   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={event => this.handleUserNameChange(event)} value={this.state.username} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={event => this.handlePasswordChange(event)} value={this.state.password} />
           </label>
         </div>
         <div>
